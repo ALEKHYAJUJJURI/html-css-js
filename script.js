e2ee3cbeba39acac2cf0b7d053e7e8cab2487d95
Alekhya <jujjurialekhya123@gmail.com>
2024-09-30 23:21:54 +0530

    fourth commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function abc(){
+    window.document.images[0].src="images/image_2.jpg";
+    window.document.forms[0].elements[1].value='Register';
+    window.document.forms[1].elements[1].value='Login';
+}
