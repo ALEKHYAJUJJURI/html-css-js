cd66822476e318b23b99a1bf3dd3378902d219aa
Alekhya <jujjurialekhya123@gmail.com>
2024-10-03 23:53:53 +0530

    commit-7

--- src/pattern.js
+++ src/pattern.js
@@ -13,3 +13,56 @@ function onSubmitClickFun(){
     }
   }
 }
+
+
+
+
+var data = ['delhi','hyd']
+function LoadCities(){
+  document.getElementById('lstSelect').innerHTML = ""
+  data.map(city=>{
+    let opt = document.createElement('option');
+    opt.value = city 
+    opt.text = city 
+    document.getElementById('lstSelect').appendChild(opt)
+  })
+  document.getElementById('cityCount').innerHTML = `Total number of Cities : ${data.length}`
+}
+function bodyLoad(){
+  LoadCities();
+}
+
+function AddClick(){
+  var inputEle = document.getElementById('inputTxt').value;
+ if(inputEle===""){
+  document.getElementById('errorMssg').innerHTML = "Please Enter city".fontcolor('red')
+ } else{
+  data.push(inputEle)
+  LoadCities()
+  alert(`${inputEle} is successfully added.`)
+  document.getElementById('errorMssg').innerHTML =""
+  document.getElementById('inputTxt').value = ""
+ }
+ 
+}
+function SortAsc(){
+  data.sort();
+  LoadCities();
+}
+function SortDsc(){
+  data.sort();
+  data.reverse();
+  LoadCities();
+}
+function RemoveClick(){
+  console.log(document.getElementById('lstSelect').value) 
+  var selectedCity = document.getElementById('lstSelect').value; 
+  var indexOfCity = data.indexOf(selectedCity) 
+  data.splice(indexOfCity,1);
+  LoadCities()
+  
+}
+function ClearClick(){
+data.length = 0
+LoadCities()
+}
