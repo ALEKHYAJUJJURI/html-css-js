5e7624890b9215551dd5b4df7cf2b8e1396f8b09
Alekhya <jujjurialekhya123@gmail.com>
2024-10-06 22:32:19 +0530

    commit-8

--- src/pattern.js
+++ src/pattern.js
@@ -28,8 +28,42 @@ function LoadCities(){
   })
   document.getElementById('cityCount').innerHTML = `Total number of Cities : ${data.length}`
 }
+
+function mainFun(){
+  let mainEle = document.getElementById('main');
+  let url = 'https://fakestoreapi.com/products'
+  fetch(url)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(dataJson){
+    console.log(dataJson)
+    dataJson.map(d=>{
+      let div = document.createElement('div');
+      div.classList = 'd-flex justify-content-center';
+      div.innerHTML = ` 
+        <div class="card w-50 mb-2">
+      <div class="card-header">
+        <span>${d.title}</span>
+      </div>
+      <div class="card-body">
+        <img src='${d.image}' width="100px"/>
+      </div>
+      <div class="card-footer p-2">
+        <div><b>${d.price}</b><span class="bg-success mt-2 p-2 rounded text-white"><span>${d.rating.rate}</span> <span class="pe-2 bi bi-star-fill">${d.rating.count} reviews</span></span></div>
+      </div>
+    </div>
+      `
+      mainEle.appendChild(div)
+    })
+  })
+
+}
+
+
 function bodyLoad(){
   LoadCities();
+  mainFun();
 }
 
 function AddClick(){
